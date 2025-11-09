620fa3f4a2b3b2c2a6b8d8b5a1026d87ec279350
bee-gy <beegy.net@gmail.com>
2025-11-09 09:03:46 +0000

    fix(web-main): correct nginx sub_filter to use actual site-id value

--- apps/web-main/docker-entrypoint.sh
+++ apps/web-main/docker-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -e
 
-# Set environment variable for nginx sub_filter
-export RYBBIT_SITE_ID_VALUE="${RYBBIT_SITE_ID:-default-site-id}"
+# Get Rybbit Site ID from environment
+RYBBIT_SITE_ID_VALUE="${RYBBIT_SITE_ID:-default-site-id}"
 
 echo "Starting nginx with Rybbit Site ID: ${RYBBIT_SITE_ID_VALUE}"
 
-# Replace environment variable in nginx config
+# Replace RYBBIT_SITE_ID_VALUE in nginx config template
+# This replaces ${RYBBIT_SITE_ID_VALUE} with the actual site-id value
 envsubst '${RYBBIT_SITE_ID_VALUE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
