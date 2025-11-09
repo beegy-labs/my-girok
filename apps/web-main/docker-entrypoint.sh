29135cc3e7da38af06daad5852b8f97d8cafe05a
bee-gy <beegy.net@gmail.com>
2025-11-09 08:56:58 +0000

    fix(web-main): use nginx sub_filter for runtime Rybbit site-id injection

--- apps/web-main/docker-entrypoint.sh
+++ apps/web-main/docker-entrypoint.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 set -e
 
-# Replace environment variables in index.html
-# This allows runtime configuration without rebuilding the image
-echo "Replacing environment variables in index.html..."
+# Set environment variable for nginx sub_filter
+export RYBBIT_SITE_ID_VALUE="${RYBBIT_SITE_ID:-default-site-id}"
 
-# Default values if not set
-RYBBIT_SITE_ID="${RYBBIT_SITE_ID:-default-site-id}"
+echo "Starting nginx with Rybbit Site ID: ${RYBBIT_SITE_ID_VALUE}"
 
-# Use envsubst to replace variables in index.html
-# Write to /tmp first (writable by nginx user), then move
-envsubst '${RYBBIT_SITE_ID}' < /usr/share/nginx/html/index.html.template > /tmp/index.html
-mv /tmp/index.html /usr/share/nginx/html/index.html
-
-echo "Environment variables replaced successfully"
-echo "RYBBIT_SITE_ID: ${RYBBIT_SITE_ID}"
+# Replace environment variable in nginx config
+envsubst '${RYBBIT_SITE_ID_VALUE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g "daemon off;"
